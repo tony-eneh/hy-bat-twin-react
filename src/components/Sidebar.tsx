c5a8e140ebae28617ffb581d3dbd462f3dda6a9a
Tony Eneh <30419946+tony-eneh@users.noreply.github.com>
2023-12-08 19:33:02 +0100

    bug: show blank for settings tab on sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -35,7 +35,7 @@ export function Sidebar(props: Props) {
       <nav className="flex items-center w-full">
         {['Architecture', 'Settings'].map((tab) => (
           <span
-            className={`flex-grow border-b-8 text-center p-1 cursor-pointer ${
+            className={`flex-1 border-b-8 text-center p-1 cursor-pointer ${
               openTab === tab.toLowerCase()
                 ? 'border-b-highlight'
                 : 'border-b-black'
@@ -50,80 +50,82 @@ export function Sidebar(props: Props) {
         ))}
       </nav>
 
-      <ul className="my-4">
-        <li>
-          <Link to={'/'} className="flex items-center gap-2 pl-7 my-2">
-            <img src={searchIcon} alt="" />
-            System Overview
-          </Link>
-        </li>
-        <li>
-          <div
-            className="flex items-center gap-2 pl-3 my-2 cursor-pointer select-none"
-            onClick={() => setDeployExpanded(!deployExpanded)}
-          >
-            <TriangleIcon expanded={deployExpanded} />
-            <img src={openBoxIcon} alt="" />
-            Deployment
-          </div>
-          <ul
-            className={`pl-3 overflow-hidden transition-all ${
-              deployExpanded ? 'h-72' : 'h-0'
-            }`}
-          >
-            {batteries.map((battery) => (
-              <li key={battery.id}>
-                <Link
-                  to={`/battery/${battery.id}`}
-                  className="flex items-center gap-2 pl-3 my-2"
-                  onClick={() =>
-                    setBatteryExpanded({
-                      [battery.id]: !batteryExpanded[battery.id],
-                    })
-                  }
-                >
-                  <TriangleIcon expanded={batteryExpanded[battery.id]} />
-                  <img src={batteryIcon} alt="" className="h-6" />
-                  {battery.name}
-                </Link>
-                <ul
-                  className={`pl-3 overflow-hidden transition-all ${
-                    batteryExpanded[battery.id] ? 'h-32' : 'h-0'
-                  }`}
-                >
-                  <li className="h-8">
-                    <Link
-                      to={`/battery/${battery.id}?graph=temperature`}
-                      className="flex items-center gap-2 pl-3 my-2"
-                    >
-                      <img src={cogIcon} alt="" className="h-6" />
-                      Temperature
-                    </Link>
-                  </li>
-                  <li className="h-8">
-                    <Link
-                      to={`/battery/${battery.id}?graph=voltage`}
-                      className="flex items-center gap-2 pl-3 my-2"
-                    >
-                      <img src={cogIcon} alt="" className="h-6" />
-                      Voltage
-                    </Link>
-                  </li>
-                  <li className="h-8">
-                    <Link
-                      to={`/battery/${battery.id}?graph=current`}
-                      className="flex items-center gap-2 pl-3 my-2"
-                    >
-                      <img src={cogIcon} alt="" className="h-6" />
-                      Current
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-            ))}
-          </ul>
-        </li>
-      </ul>
+      {openTab === 'architecture' && (
+        <ul className="my-4">
+          <li>
+            <Link to={'/'} className="flex items-center gap-2 pl-7 my-2">
+              <img src={searchIcon} alt="" />
+              System Overview
+            </Link>
+          </li>
+          <li>
+            <div
+              className="flex items-center gap-2 pl-3 my-2 cursor-pointer select-none"
+              onClick={() => setDeployExpanded(!deployExpanded)}
+            >
+              <TriangleIcon expanded={deployExpanded} />
+              <img src={openBoxIcon} alt="" />
+              Deployment
+            </div>
+            <ul
+              className={`pl-3 overflow-hidden transition-all ${
+                deployExpanded ? 'h-72' : 'h-0'
+              }`}
+            >
+              {batteries.map((battery) => (
+                <li key={battery.id}>
+                  <Link
+                    to={`/battery/${battery.id}`}
+                    className="flex items-center gap-2 pl-3 my-2"
+                    onClick={() =>
+                      setBatteryExpanded({
+                        [battery.id]: !batteryExpanded[battery.id],
+                      })
+                    }
+                  >
+                    <TriangleIcon expanded={batteryExpanded[battery.id]} />
+                    <img src={batteryIcon} alt="" className="h-6" />
+                    {battery.name}
+                  </Link>
+                  <ul
+                    className={`pl-3 overflow-hidden transition-all ${
+                      batteryExpanded[battery.id] ? 'h-32' : 'h-0'
+                    }`}
+                  >
+                    <li className="h-8">
+                      <Link
+                        to={`/battery/${battery.id}?graph=temperature`}
+                        className="flex items-center gap-2 pl-3 my-2"
+                      >
+                        <img src={cogIcon} alt="" className="h-6" />
+                        Temperature
+                      </Link>
+                    </li>
+                    <li className="h-8">
+                      <Link
+                        to={`/battery/${battery.id}?graph=voltage`}
+                        className="flex items-center gap-2 pl-3 my-2"
+                      >
+                        <img src={cogIcon} alt="" className="h-6" />
+                        Voltage
+                      </Link>
+                    </li>
+                    <li className="h-8">
+                      <Link
+                        to={`/battery/${battery.id}?graph=current`}
+                        className="flex items-center gap-2 pl-3 my-2"
+                      >
+                        <img src={cogIcon} alt="" className="h-6" />
+                        Current
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </li>
+        </ul>
+      )}
     </aside>
   );
 }
