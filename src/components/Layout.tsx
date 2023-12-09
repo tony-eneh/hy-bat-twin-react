2ff1b89d946489bb1589add2b564c1f2beb89498
Tony Eneh <30419946+tony-eneh@users.noreply.github.com>
2023-12-09 14:56:27 +0100

    feat: page layout gradient backgrounds

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,10 +9,15 @@ export function Layout({ children }: Props) {
   const [open, setOpen] = useState(true);
 
   return (
-    <div className="">
+    <div className="flex flex-col min-h-screen">
       <Header toggle={() => setOpen(!open)} />
-      <div>
-        <Sidebar open={open} />
+      <div className="flex flex-grow relative">
+        {/* background gradients */}
+        <div className="absolute top-2 right-2 w-56 h-56 rounded-full opacity-50 -z-20 bg-gradient-to-tr from-[#a18cd1] to-[#fbc2eb]"></div>
+        <div className="absolute top-1/2 left-64 w-56 h-56 rounded-full opacity-50 -z-20 bg-gradient-to-t from-[#a1c4fd] to-[#c2e9fb]"></div>
+        <div className="absolute bottom-2 right-0 w-56 h-56 rounded-full opacity-50 -z-20 bg-gradient-to-t from-[#e0c3fc] to-[#8ec5fc]"></div>
+
+        <Sidebar open={open} setOpen={setOpen} />
         {children}
       </div>
     </div>
