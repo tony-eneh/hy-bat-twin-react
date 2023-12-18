b7d6d590d51727664c2e9cba905bb753ef40d6b1
Tony Eneh <30419946+tony-eneh@users.noreply.github.com>
2023-12-18 13:51:13 +0100

    chore: install and configure toaster

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,25 @@
+import { ToastOptions, toast } from 'react-toastify';
+
 export const headers = {
   Accept: 'application/json',
   'Content-Type': 'application/json',
+};
+
+// toasts
+const toastSettings: ToastOptions = {
+  position: 'top-right',
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'light',
+};
+
+export const toaster = {
+  success: (message: string) => toast.success(message, toastSettings),
+  info: (message: string) => toast.info(message, toastSettings),
+  warning: (message: string) => toast.warning(message, toastSettings),
+  error: (message: string) => toast.error(message, toastSettings),
 };
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.ts';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
+    <ToastContainer />
   </React.StrictMode>
 );
